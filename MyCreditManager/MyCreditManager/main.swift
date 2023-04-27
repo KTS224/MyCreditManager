cbd14d4f752a5a50c462d66201e9f1388fd0ca6a
KTS224 <ts4683@gmail.com>
2023-04-27 22:36:31 +0900

    리펙토링
    - 함수로 분리 후 스위치문으로 변경

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -10,172 +10,189 @@ import Foundation
 
 struct Student {
     var name: String
-    var subjects: [String: String] = [:]    // ex) [Swift: A+]
+    var subjects: [String: String] = [:]    // 과목: 등급 ex) [Swift: A+]
     var score: Double = 0                   // 점수의 총합
 }
 
 var students: [Student] = []
 
-while true {
+loop: while true {
     print("원하는 기능을 입력해주세요")
     print("1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
 
-    var input = readLine()!
+    let command = readLine()!
+    
+    switch command {
+    case "1":
+        addStudent()
+        continue
+    case "2":
+        deleteStudent()
+        continue
+    case "3":
+        addOrChangeScore()
+        continue
+    case "4":
+        deleteScore()
+        continue
+    case "5":
+        printScore()
+        continue
+    case "X":
+        print("프로그램을 종료합니다...")
+        break loop
+    default:
+        print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.")
+        continue
+    }
+}
 
-    // MARK: - 학생 추가
-    if input == "1" {
-        print("추가할 학생의 이름을 입력해주세요")
-        input = readLine()!
+// MARK: - 학생 추가
+func addStudent() {
+    print("추가할 학생의 이름을 입력해주세요")
+    let input = readLine()!
+    
+    guard checkWhiteSpaces(input) else {
+        print("입력이 잘못되었습니다. 다시 확인해주세요.")
+        return
+    }
+    
+    var isDuplicated: Bool = false
+    
+    for studient in students {
+        if studient.name == input {
+            isDuplicated = true
+            break
+        }
+    }
+
+    if isDuplicated {
+        print("\(input)은 이미 존재하는 학생입니다. 추가하지 않습니다.")
+    } else {
+        students.append(Student(name: input))
+        print("\(input) 학생을 추가했습니다.")
+    }
+}
+
+// MARK: - 학생 삭제
+func deleteStudent() {
+    print("삭제할 학생의 이름을 입력해주세요")
+    
+    let input = readLine()!
+    var isExist: Bool = false
+    var studentLocation = 0
+
+    for i in 0..<students.count {
+        if students[i].name == input {
+            print("\(input) 학생을 삭제하였습니다.")
+            isExist = true
+            studentLocation = i
+            break
+        }
+    }
+
+    if isExist {
+        students.remove(at: studentLocation)
+    } else {
+        print("\(input) 학생을 찾지 못했습니다.")
+    }
+}
+
+// MARK: - 성적 추가
+func addOrChangeScore() {
+    print("성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.")
+    print("입력예) Mickey Swift A+")
+    print("만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.")
+    
+    let inputt = readLine()!.split(separator: " ").map{ $0 }
+    if inputt.count != 3 {
+        print("입력이 잘못 되었습니다. 다시 확인해주세요.")
+    } else {
+        let name = String(inputt[0])
+        let subject = String(inputt[1])
+        let grade = String(inputt[2])
         
-        if checkWhiteSpaces(input) {
-            print("입력이 잘못되었습니다. 다시 확인해주세요.")
-            continue
-        }
-        
-        var isDuplicated: Bool = false
-
-        students.forEach { student in
-            if student.name == input {
-                isDuplicated = true
-            }
-        }
-
-        if isDuplicated {
-            print("\(input)은 이미 존재하는 학생입니다. 추가하지 않습니다.")
-        } else {
-            students.append(Student(name: input))
-            print("\(input) 학생을 추가했습니다.")
-        }
-        continue
-    }
-
-    // MARK: - 학생 삭제
-    if input == "2" {
-        print("삭제할 학생의 이름을 입력해주세요")
-        input = readLine()!
-        var isExist: Bool = false
-        var studentLocation = 0
-
-        for i in 0..<students.count {
-            if students[i].name == input {
-                print("\(input) 학생을 삭제하였습니다.")
-                isExist = true
-                studentLocation = i
-                break
-            }
-        }
-
-        if isExist {
-            students.remove(at: studentLocation)
-        } else {
-            print("\(input) 학생을 찾지 못했습니다.")
-        }
-        continue
-    }
-
-    // MARK: - 성적 추가
-    if input == "3" {
-        print("성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.")
-        print("입력예) Mickey Swift A+")
-        print("만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.")
-        let inputt = readLine()!.split(separator: " ").map{ $0 }
-        if inputt.count != 3 {
+        guard checkCorrectName(input: name) && checkCorrectGrade(input: grade) else {
             print("입력이 잘못 되었습니다. 다시 확인해주세요.")
-        } else {
-            let name = String(inputt[0])
-            let subject = String(inputt[1])
-            let grade = String(inputt[2])
-            
-            guard checkCorrectName(input: name) && checkCorrectGrade(input: grade) else {
-                print("입력이 잘못 되었습니다. 다시 확인해주세요.")
-                continue
-            }
-
-            for i in 0..<students.count {
-                if students[i].name == name {
-                    if students[i].subjects[subject] != nil { // 기존 성적이 있을 경우
-                        students[i].score -= changeGradeToScore(students[i].subjects[subject] ?? "") // 기존점수 제거
-                    }
-                    students[i].score += changeGradeToScore(grade)
-                    students[i].subjects[subject] = grade
-                    print("\(name) 학생의 \(subject) 과목이 \(grade)로 추가(변경)되었습니다.")
-                    break
-                }
-            }
+            return
         }
-        continue
-    }
-
-    // MARK: - 성적 삭제
-    if input == "4" {
-        print("성적을 삭제할 학생의 이름, 과목 이름을 띄어쓰기로 구분하여 차례로 작성해주세요.")
-        print("입력예) Mickey Swift")
-        let inputt = readLine()!.split(separator: " ").map{ $0 }
-        if inputt.count != 2 {
-            print("입력이 잘못 되었습니다. 다시 확인해주세요.")
-        } else {
-            let name = String(inputt[0])
-            let subject = String(inputt[1])
-            
-            guard checkCorrectName(input: name) else {
-                print("\(name) 학생을 찾지 못했습니다.")
-                continue
-            }
-            
-            guard checkCorrectName(input: name) && checkCorrentSubject(input: subject) else {
-                print("입력이 잘못 되었습니다. 다시 확인해주세요.")
-                continue
-            }
-
-            for i in 0..<students.count {
-                if students[i].name == name {
-                    students[i].score -= changeGradeToScore(students[i].subjects[subject] ?? "")
-                    students[i].subjects[subject] = nil
-                    print("\(name) 학생의 \(subject) 과목의 성적이 삭제되었습니다.")
-                    break
-                }
-            }
-        }
-        continue
-    }
-
-    // MARK: - 평점 보기
-    if input == "5" {
-        print("평점을 알고싶은 학생의 이름을 입력해주세요")
-        let name = readLine()!
-        var isExist: Bool = false
 
         for i in 0..<students.count {
             if students[i].name == name {
-                isExist = true
-
-                for subject in students[i].subjects {
-                    print("\(subject.key): \(subject.value)")
+                if students[i].subjects[subject] != nil { // 기존 성적이 있을 경우
+                    students[i].score -= changeGradeToScore(students[i].subjects[subject] ?? "") // 기존점수 제거
                 }
-
-                let score = round(students[i].score/Double(students[i].subjects.count) * 100) / 100
-                // 소수점 지우기 4.0 -> 4 로 만들기 위해 처리
-                if score.truncatingRemainder(dividingBy: 1) == 0 {
-                    print("평점 : \(Int(score))")
-                } else {
-                    print("평점 : \(score)")
-                }
+                students[i].score += changeGradeToScore(grade)
+                students[i].subjects[subject] = grade
+                print("\(name) 학생의 \(subject) 과목이 \(grade)로 추가(변경)되었습니다.")
                 break
             }
         }
-        
-        if !isExist {
-            print("\(name) 학생을 찾지 못했습니다.")
-        }
-        continue
     }
+}
 
-    if input == "X" {
-        print("프로그램을 종료합니다...")
-        break
+// MARK: - 성적 삭제
+func deleteScore() {
+    print("성적을 삭제할 학생의 이름, 과목 이름을 띄어쓰기로 구분하여 차례로 작성해주세요.")
+    print("입력예) Mickey Swift")
+    
+    let inputt = readLine()!.split(separator: " ").map{ $0 }
+    if inputt.count != 2 {
+        print("입력이 잘못 되었습니다. 다시 확인해주세요.")
+    } else {
+        let name = String(inputt[0])
+        let subject = String(inputt[1])
+        
+        guard checkCorrectName(input: name) else {
+            print("\(name) 학생을 찾지 못했습니다.")
+            return
+        }
+        
+        guard checkCorrectName(input: name) && checkCorrentSubject(input: subject) else {
+            print("입력이 잘못 되었습니다. 다시 확인해주세요.")
+            return
+        }
+
+        for i in 0..<students.count {
+            if students[i].name == name {
+                students[i].score -= changeGradeToScore(students[i].subjects[subject] ?? "")
+                students[i].subjects[subject] = nil
+                print("\(name) 학생의 \(subject) 과목의 성적이 삭제되었습니다.")
+                break
+            }
+        }
+    }
+}
+
+// MARK: - 평점 보기
+func printScore() {
+    print("평점을 알고싶은 학생의 이름을 입력해주세요")
+    
+    let name = readLine()!
+    var isExist: Bool = false
+
+    for i in 0..<students.count {
+        if students[i].name == name {
+            isExist = true
+
+            for subject in students[i].subjects {
+                print("\(subject.key): \(subject.value)")
+            }
+
+            let score = round(students[i].score/Double(students[i].subjects.count) * 100) / 100
+            // 소수점 지우기 4.0 -> 4 로 만들기 위해 처리
+            if score.truncatingRemainder(dividingBy: 1) == 0 {
+                print("평점 : \(Int(score))")
+            } else {
+                print("평점 : \(score)")
+            }
+            break
+        }
     }
     
-    print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.")
+    if !isExist {
+        print("\(name) 학생을 찾지 못했습니다.")
+    }
 }
 
 func changeGradeToScore(_ grade: String) -> Double {
@@ -205,10 +222,10 @@ func checkWhiteSpaces(_ input: String) -> Bool {
     let afterTrim = input.trimmingCharacters(in: .whitespacesAndNewlines)
     
     if input == "" || input != afterTrim  { // 공백 입력 처리
-        return true
+        return false
     }
     
-    return false
+    return true
 }
 
 func checkCorrectName(input: String) -> Bool {
